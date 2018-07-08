74bb7f1dfa285d89173069a5e2c3412d165006cd
Toby Hoshman <t.hoshman@gmail.com>
2018-07-08 12:32:24 -0700

    add O class

---
+++ client/src/tetraminos/O.js
@@ -0,0 +1,64 @@
+import Shape from './Shape';
+/* borrowed from Joe Snowden
+ * NORTH
+ * 🐯🔄🐯
+ * 🐯️️
+ * WEST
+ * 🐯
+ * 🐯🔄
+ * 🐯🐯
+ * SOUTH
+ * ◽️🔄️🐯
+ * 🐯🐯🐯
+ * EAST
+ * 🐯🐯
+ * ◽️🔄
+ * ◽️🐯
+ */
+
+export default class L extends Shape {
+  constructor() {
+    super();
+    const [row, column] = this;
+    this.hub = [row, column];
+    this.blockOne = [row, column + 1];
+    this.blockTwo = [row, column];
+    this.blockThree = [row + 1, column + 1];
+    this.blockFour = [row + 1, column];
+  }
+  predictBlockLocations(move) {
+    const {
+      blockOne,
+      blockTwo,
+      blockThree,
+      blockFour,
+    } = this;
+    let one;
+    let two;
+    let three;
+    let four;
+    switch (move) {
+      case 'descend':
+        one = [blockOne[0] + 1, blockOne[1]];
+        two = [blockTwo[0] + 1, blockTwo[1]];
+        three = [blockThree[0] + 1, blockThree[1]];
+        four = [blockFour[0] + 1, blockFour[1]];
+        break;
+      case 'left':
+        one = [blockOne[0], blockOne[1] - 1];
+        two = [blockTwo[0], blockTwo[1] - 1];
+        three = [blockThree[0], blockThree[1] - 1];
+        four = [blockFour[0], blockFour[1] - 1];
+        break;
+      case 'right':
+        one = [blockOne[0], blockOne[1] + 1];
+        two = [blockTwo[0], blockTwo[1] + 1];
+        three = [blockThree[0], blockThree[1] + 1];
+        four = [blockFour[0], blockFour[1] + 1];
+        break;
+      default:
+        return [blockOne, blockTwo, blockThree, blockFour];
+    }
+    return [one, two, three, four];
+  }
+}
